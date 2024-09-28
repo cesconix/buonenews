63ecb66256b8db29971f1decb2e1f0775ac9838e
Francesco Pasqua <cesconix@me.com>
2024-09-28 18:59:31 +0200

    chore: minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,12 +10,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "hybrid",
+  output: "server",
   adapter: vercel({ isr: { expiration: 60 } }),
   image: {
     domains: ["www.datocms-assets.com"],
-  },
-  redirects: {
-    "/": "/home",
   },
 });
